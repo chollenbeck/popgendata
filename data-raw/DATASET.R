6796dd4f851cda009ac93255700505ae104e4c8b
Chris Hollenbeck <chollenbeck07@gmail.com>
2021-10-28 15:39:55 -0500

    Update data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,6 +4,10 @@ twentygen <- readr::read_rds("data-raw/twentygen.rds")
 
 usethis::use_data(twentygen, overwrite = TRUE, compress = "gzip")
 
-reddrum <- readr::read_rds("data-raw/red_drum_rad.rds")
+drumpop <- readr::read_rds("data-raw/red_drum_rad.rds")
+
+reddrum_map <- readr::read_tsv("data-raw/consensus_map.tab")
+
+reddrum <- list(geno = drumpop, map = reddrum_map)
 
 usethis::use_data(reddrum, overwrite = TRUE, compress = "gzip")
